81c9c00c55d6e53fc4acfe4bf107c8be1b25a90b
mnl187 <mnl187@gmail.com>
2023-01-28 18:27:12 +0100

    dodanie skryptu pobierającego ikony w index.html oraz ikon medii społecznosciowych do footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./Footer.css"
+
 export const Footer = () => {
     return (
         <footer>
@@ -8,22 +10,22 @@ export const Footer = () => {
                     <div className="col">
                         <h3>Skotaktuj się z nami</h3>
                         <ul>
-                            <li>Addres: 123 Main St</li>
+                            <li>Adres: Ulica 123</li>
                             <li>Telefon: 555-555-5555</li>
                             <li>Email: info@optika.com</li>
                         </ul>
                     </div>
                     <div className="col">
-                        <h3>Follow Us</h3>
+                        <h3>Śledź nas</h3>
                         <ul>
                             <li>
-                                <a href="#">Facebook</a>
+                                <a href="https://aboutblank.pl/"><i className="fa-brands fa-facebook"></i> Facebook</a>
                             </li>
                             <li>
-                                <a href="#">Twitter</a>
+                                <a href="https://aboutblank.pl/"><i className="fa-brands fa-twitter"></i> Twitter</a>
                             </li>
                             <li>
-                                <a href="#">Instagram</a>
+                                <a href="https://aboutblank.pl/"><i className="fa-brands fa-instagram"></i> Instagram</a>
                             </li>
                         </ul>
                     </div>
